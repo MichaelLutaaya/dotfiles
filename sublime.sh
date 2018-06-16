b2cc28ad233e9c90bb3268f324c747d355cc4f9e
Michael Lutaaya <michael@lutaaya.com>
2018-06-16 16:14:07 -0400

    Fix linking of Sublime Text preferences

--- sublime.sh
+++ sublime.sh
@@ -2,9 +2,10 @@
 
 set -e
 
+SOURCE_FILE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/sublime/"
 TARGET_FILE=$HOME/"Library/Application Support/Sublime Text 3/Packages/User"
 
 osascript -e 'quit app "Sublime Text"'
 
 rm -rf "$TARGET_FILE"
-ln -Ffs sublime/ "$TARGET_FILE"
+ln -Ffs "$SOURCE_FILE" "$TARGET_FILE"
